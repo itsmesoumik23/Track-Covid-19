d3505fcbba87eb9bae5343951ee22183ff672c4c
Soumik Mukherjee <77982862+itsmesoumik23@users.noreply.github.com>
2021-07-13 00:04:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,27 +19,55 @@ app.use(express.static("public"))
 
 
 app.get("/", function(req, res){
+    globalTotalConfirmed = 0;
+    globalTotalDeaths = 0;
+    globalTotalRecovered = 0;
 
-    https.get('https://www.trackcorona.live/api/countries', function(response){
+    https.get('https://api.covid19api.com/summary', function(response){
+        // var chunks = []
         var Items = ''
+        
         response.on("data", function(data){
             Items += data;
         })
+
         response.on("end", function(){
+            // const body = Buffer.concat(chunks)
             try{
                 var mainData = JSON.parse(Items)
-                var j = 0
-                while (j < 230){
-                    globalTotalConfirmed = globalTotalConfirmed + mainData.data[j].confirmed
-                    globalTotalDeaths = globalTotalDeaths + mainData.data[j].dead
-                    globalTotalRecovered = globalTotalRecovered + mainData.data[j].recovered
-                    j = j+1
+                globalTotalConfirmed = mainData.Global.TotalConfirmed
+                globalTotalRecovered = mainData.Global.TotalRecovered
+                globalTotalDeaths = mainData.Global.TotalDeaths
+                
+                function comparator(a, b){
+                    return a-b
                 }
-//                 console.log(mainData.data.length, globalTotalConfirmed, globalTotalDeaths, globalTotalRecovered);
+
                 var i = 0;
+                var all = new Array();
+                const topFive = new Array();
+                var j = 0
+                // console.log(mainData.Countries.length);
+                while (j < 192){
+                    all.push(Number(mainData.Countries[j].TotalConfirmed))
+                    j++;
+                }
+                // console.log(all);
+                var sorted = all.sort((a, b)=>{
+                    return b-a;
+                })
+                // console.log(sorted);
+                // console.log(all);
+                // console.log(all.length);
+                x = 0
+                while (x < 5){
+                    topFive.push(all[x])
+                    x++;
+                }
+                console.log(topFive);
                 if (nc !== ""){
-                    while (i < 230) {
-                        if ((mainData.data[i].location).toLowerCase().indexOf(nc.toLowerCase()) !== -1){
+                    while (i < 192) {
+                        if ((mainData.Countries[i].Slug).toLowerCase().indexOf(nc.toLowerCase()) !== -1){
                             flag = 0;
                             breakCalled=true
                             break
@@ -47,14 +75,18 @@ app.get("/", function(req, res){
                         i = i + 1;
                     }
                 }
-                if (i === 230){
+                if (i === 192){
                     i = 0;
                     searchMiss = true;
                 }
-                countryTotalConfirmed = mainData.data[i].confirmed
-                countryTotalDeaths = mainData.data[i].dead
-                countryTotalRecovered = mainData.data[i].recovered
-                const CN = mainData.data[i].location
+
+                // const countryNewConfirmed = data.Countries[i].NewConfirmed
+                countryTotalConfirmed = mainData.Countries[i].TotalConfirmed
+                // const countryNewDeaths = data.Countries[i].NewDeaths
+                countryTotalDeaths = mainData.Countries[i].TotalDeaths
+                // const countryNewRecovered = data.Countries[i].NewRecovered
+                countryTotalRecovered = mainData.Countries[i].TotalRecovered
+                const CN = mainData.Countries[i].Country
                 if (nc !== ""){
                     nc = ""
                     i = 0
@@ -77,8 +109,7 @@ app.get("/", function(req, res){
             } catch (error){
                 const data = Items.toString()
                 console.log(data);
-            } 
-
+            }
         })
 
         response.on("error", function(error){
@@ -90,6 +121,7 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
     nc = req.body.InputCountry
+    // console.log(nc);
     res.redirect("/")
 })
 
